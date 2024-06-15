77b81aa9a4f240c5de0aaa2d2c832078310ae674
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-15 18:55:11 +0200

    commit before pull

--- src/app/pages/user-page/user-page.component.ts
+++ src/app/pages/user-page/user-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfilComponent } from './components/profil/profil.component';
 
 @Component({
   selector: 'app-user-page',
   standalone: true,
-  imports: [],
+  imports: [ProfilComponent],
   templateUrl: './user-page.component.html',
   styleUrl: './user-page.component.scss',
 })
