9db61a6b77bb1e27c76ff37539389d4617606d2a
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-18 15:52:12 +0200

    add filterBar component to the homepage with style and responsiveness

---
+++ src/app/pages/homepage/components/filter-bar/filter-bar.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterBarComponent } from './filter-bar.component';
+
+describe('FilterBarComponent', () => {
+  let component: FilterBarComponent;
+  let fixture: ComponentFixture<FilterBarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FilterBarComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FilterBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
