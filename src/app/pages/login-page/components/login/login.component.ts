686ae506a7733d03ac66fe73f5219a08a1ab803c
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-21 13:50:43 +0200

    commit before branch swap

---
+++ src/app/pages/login-page/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  pwdHidden = true;
+  toggleConfirPwdView() {
+    this.pwdHidden = !this.pwdHidden;
+  }
+}
--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
