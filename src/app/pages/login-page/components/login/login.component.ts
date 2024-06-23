b91bcecb787ccbd6dbfe3c207969167da44b7cf4
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-23 14:18:53 +0200

    add login form and responsive design

--- src/app/pages/login-page/components/login/login.component.ts
+++ src/app/pages/login-page/components/login/login.component.ts
@@ -1,17 +1,36 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  fb: FormBuilder = inject(FormBuilder);
   pwdHidden = true;
+  isFocused = false;
   toggleConfirPwdView() {
     this.pwdHidden = !this.pwdHidden;
+  }
+
+  public loginForm = this.fb.group({
+    email: [''],
+    password: [''],
+  });
+  onSubmit(): void {
+    console.warn(this.loginForm.value);
+    console.warn('to the backend');
+  }
+
+  onFocus() {
+    this.isFocused = true;
+  }
+  onBlur() {
+    this.isFocused = false;
   }
 }
