3b7557bcfc0c1825c82174ca9dd7e263036501f7
wajih-ayachi <69911574+wajih-ayachi@users.noreply.github.com>
2024-06-24 16:52:15 +0200

    add event page  add eventcomponent

---
+++ src/app/pages/event-page/event-page-event-card/event-page-event-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-page-event-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-page-event-card.component.html',
+  styleUrl: './event-page-event-card.component.scss',
+})
+export class EventPageEventCardComponent {}
