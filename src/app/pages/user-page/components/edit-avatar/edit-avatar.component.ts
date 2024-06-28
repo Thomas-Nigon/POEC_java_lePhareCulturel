94ebce40aa489c77280163246b77e3dd92904817
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-28 09:53:52 +0200

    add edit avatar style

--- src/app/pages/user-page/components/edit-avatar/edit-avatar.component.ts
+++ src/app/pages/user-page/components/edit-avatar/edit-avatar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AvatarService } from '../../../../shared/services/avatar.service';
+import { Avatar } from '../../../../models/avatar.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-avatar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './edit-avatar.component.html',
   styleUrl: './edit-avatar.component.scss',
 })
-export class EditAvatarComponent {}
+export class EditAvatarComponent implements OnInit {
+  private avatarService = inject(AvatarService);
+  avatarList!: Avatar[];
+
+  ngOnInit() {
+    this.avatarService.getAvatarList().subscribe(data => {
+      this.avatarList = data;
+    });
+  }
+}
