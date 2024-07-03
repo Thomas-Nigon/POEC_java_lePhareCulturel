68bc40b95181ade5aa8a3fd72e1e17f4bc5c0c50
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-07-03 20:27:40 +0200

    fix error in console where props where not properly transmitted before page initialization

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,20 +1,23 @@
 import { Component, Input, inject } from '@angular/core';
 import { EventInterface } from '../../models/event.model';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.scss',
 })
 export class EventCardComponent {
   @Input() eventList: EventInterface[] = [];
   router = inject(Router);
+  targetId!: number | null;
+  onClick(event: Event) {
+    const clickedId: string | null = (event.target as HTMLInputElement).getAttribute('id');
+    this.targetId = clickedId ? parseInt(clickedId) - 1 : 0;
 
-  onClick() {
-    this.router.navigate(['/event', 1]).catch((error: unknown) => {
+    this.router.navigate(['/event', this.targetId]).catch((error: unknown) => {
       console.error(error);
     });
   }
--- src/app/pages/user-page/components/my-events/my-events.component.ts
+++ src/app/pages/user-page/components/my-events/my-events.component.ts
@@ -1,18 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { EventInterface } from '../../../../models/event.model';
 import { EventCardComponent } from '../../../../components/event-card/event-card.component';
-import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-my-events',
   standalone: true,
-  imports: [CommonModule, EventCardComponent],
+  imports: [CommonModule, EventCardComponent, RouterLink],
   templateUrl: './my-events.component.html',
   styleUrl: './my-events.component.scss',
 })
 export class MyEventsComponent {
-  router = inject(Router);
   incomingEvents = true;
   @Input() eventList: EventInterface[] = [];
   onClickIncoming() {
@@ -20,10 +19,5 @@ export class MyEventsComponent {
   }
   onClickpast() {
     this.incomingEvents = false;
-  }
-  onClick() {
-    this.router.navigate(['/event', 1]).catch((error: unknown) => {
-      console.error(error);
-    });
   }
 }
