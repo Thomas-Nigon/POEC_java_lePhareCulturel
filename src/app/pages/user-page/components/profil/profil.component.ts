4f782bcdcecc371ab8ae912b3ba5cf137d3ffa0f
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-07-04 14:41:25 +0200

    remove unused edit component, add text area for userdescription

--- src/app/pages/user-page/components/profil/profil.component.ts
+++ src/app/pages/user-page/components/profil/profil.component.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EditAvatarComponent } from '../edit-avatar/edit-avatar.component';
 import { MyInfoComponent } from '../my-info/my-info.component';
 import { EditPasswordComponent } from '../edit-password/edit-password.component';
-import { EditInfoComponent } from '../edit-info/edit-info.component';
 import { UserInterface } from '../../../../models/user.model';
 
 @Component({
   selector: 'app-profil',
   standalone: true,
-  imports: [RouterLink, CommonModule, EditAvatarComponent, MyInfoComponent, EditInfoComponent, EditPasswordComponent],
+  imports: [RouterLink, CommonModule, EditAvatarComponent, MyInfoComponent, EditPasswordComponent],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.scss',
 })
