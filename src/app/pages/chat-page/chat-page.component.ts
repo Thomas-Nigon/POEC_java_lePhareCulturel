4ed50ffd3e9f9187013653161df891fad02af39b
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-07-15 22:15:45 +0200

    add services to fetch json fake conversation

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SingleMessageCardComponent } from './components/single-message-card/single-message-card.component';
+import { MessagesService } from '../../shared/services/messages.service';
+import { messageInterface } from '../../models/chat.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-chat-page',
   standalone: true,
-  imports: [SingleMessageCardComponent],
+  imports: [SingleMessageCardComponent, CommonModule],
   templateUrl: './chat-page.component.html',
   styleUrl: './chat-page.component.scss',
 })
-export class ChatPageComponent {}
+export class ChatPageComponent implements OnInit {
+  messageList!: messageInterface[];
+  messageService = inject(MessagesService);
+  ngOnInit() {
+    this.messageService.getMessagesByGroup().subscribe(messages => {
+      this.messageList = messages;
+    });
+  }
+}
