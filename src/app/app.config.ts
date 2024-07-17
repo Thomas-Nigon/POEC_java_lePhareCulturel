4cf811df445fddf82cb08fc0162c55a38d5ac1c9
Samuel MICHAUX <samuel.michaux@ik.me>
2024-07-17 16:15:45 +0200

    add cookie interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { cookieInterceptor } from './shared/interceptors/cookie.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([cookieInterceptor])),
     provideIonicAngular(),
   ],
 };
