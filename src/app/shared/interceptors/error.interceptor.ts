cb0c7fc8ba63c01ce464781eaaeecdbe67be4b92
Samuel MICHAUX <samuel.michaux@ik.me>
2024-07-24 11:14:18 +0200

    Correction du problem du authInterceptor avec le refresh token qui n'était plus appelé

--- src/app/shared/interceptors/error.interceptor.ts
+++ src/app/shared/interceptors/error.interceptor.ts
@@ -2,12 +2,12 @@ import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  // const authService = inject(AuthService);
   const errorReq = req.clone({ withCredentials: true });
   return next(errorReq).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
-        // authService.handleUnauthorized();
+        // Ignore 401 errors to let authInterceptor handle them
+        return throwError(() => error);
       }
       // console.log('HTTP Error Interceptor:', error.message);
       return throwError(() => new Error(error.message));
